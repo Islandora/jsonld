73ccfc5ea6aeabf1a1774330f4436e45b4786dc0
DiegoPino <dpino@krayon.cl>
2016-09-26 09:24:44 -0300

    Clearing up debug messages
    @ruebot removed this.

--- src/JsonldServiceProvider.php
+++ src/JsonldServiceProvider.php
@@ -14,7 +14,6 @@ class JsonldServiceProvider implements ServiceModifierInterface {
    * {@inheritdoc}
    */
   public function alter(ContainerBuilder $container) {
-    error_log('hey, json ld is in the house');
     if ($container->has('http_middleware.negotiation') && is_a($container->getDefinition('http_middleware.negotiation')->getClass(), '\Drupal\Core\StackMiddleware\NegotiationMiddleware', TRUE)) {
       $container->getDefinition('http_middleware.negotiation')->addMethodCall('registerFormat', ['jsonld', ['application/ld+json']]);
     }

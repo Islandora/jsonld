b7acb80a8793a35f3d688acbf12c9334a9fc59b8
Seth Shaw <seth.shaw@unlv.edu>
2021-04-27 11:38:30 -0700

    Make rdf namespaces configurable (Issue 1520) (#53)
    * add support for configurable namespaces
    
    * Validation: prevent duplicate prefixes

--- src/Form/JsonLdSettingsForm.php
+++ src/Form/JsonLdSettingsForm.php
@@ -15,6 +15,7 @@ class JsonLdSettingsForm extends ConfigFormBase {
   const CONFIG_NAME = 'jsonld.settings';
 
   const REMOVE_JSONLD_FORMAT = 'remove_jsonld_format';
+  const RDF_NAMESPACES = 'rdf_namespaces';
 
   /**
    * {@inheritdoc}
@@ -45,7 +46,64 @@ class JsonLdSettingsForm extends ConfigFormBase {
         '#default_value' => $config->get(self::REMOVE_JSONLD_FORMAT) ? $config->get(self::REMOVE_JSONLD_FORMAT) : FALSE,
       ],
     ];
+
+    $rdf_namespaces = '';
+    foreach ($config->get('rdf_namespaces') as $namespace) {
+      $rdf_namespaces .= $namespace['prefix'] . '|' . $namespace['namespace'] . "\n";
+    }
+    $form[self::RDF_NAMESPACES] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('RDF Namespaces'),
+      '#rows' => 10,
+      '#default_value' => $rdf_namespaces,
+    ];
+
     return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    // Validate RDF Namespaces.
+    // First we stash a copy of the existing prefixes so we can find additions.
+    $previous_prefixes = [];
+    foreach ($this->config(self::CONFIG_NAME)->get('rdf_namespaces') as $namespace) {
+      $previous_prefixes[] = $namespace['prefix'];
+    }
+    $new_prefixes = [];
+
+    foreach (preg_split("/[\r\n]+/", $form_state->getValue(self::RDF_NAMESPACES)) as $line) {
+      if (empty($line)) {
+        continue;
+      }
+      $namespace = explode("|", trim($line));
+      if (empty($namespace[0]) || empty($namespace[1])) {
+        $form_state->setErrorByName(
+          self::RDF_NAMESPACES,
+          $this->t("RDF Namespace form is malformed on line '@line'",
+            ['@line' => trim($line)]
+          )
+        );
+        continue;
+      }
+      if (!in_array($namespace[0], $previous_prefixes)) {
+        $new_prefixes[] = $namespace[0];
+      }
+    }
+
+    // Check to make sure new prefixes aren't already in use.
+    $all_namespaces = rdf_get_namespaces();
+    foreach ($new_prefixes as $prefix) {
+      if (in_array($prefix, array_keys($all_namespaces))) {
+        $form_state->setErrorByName(
+          self::RDF_NAMESPACES,
+          $this->t("RDF namespace prefix '@prefix' is already mapped by a module to '@namespace'",
+            ['@prefix' => $prefix, '@namespace' => $all_namespaces[$prefix]]
+          )
+        );
+      }
+    }
   }
 
   /**
@@ -53,7 +111,24 @@ class JsonLdSettingsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $config = $this->configFactory->getEditable(self::CONFIG_NAME);
-    $config->set(self::REMOVE_JSONLD_FORMAT, $form_state->getValue(self::REMOVE_JSONLD_FORMAT))
+
+    $namespaces_array = [];
+    foreach (preg_split("/[\r\n]+/", $form_state->getValue(self::RDF_NAMESPACES)) as $line) {
+      if (empty($line)) {
+        continue;
+      }
+      $namespace = explode("|", trim($line));
+      if (!empty($namespace[0]) && !empty($namespace[1])) {
+        $namespaces_array[] = [
+          'prefix' => trim($namespace[0]),
+          'namespace' => trim($namespace[1]),
+        ];
+      }
+    }
+
+    $config
+      ->set(self::REMOVE_JSONLD_FORMAT, $form_state->getValue(self::REMOVE_JSONLD_FORMAT))
+      ->set(self::RDF_NAMESPACES, $namespaces_array)
       ->save();
     parent::submitForm($form, $form_state);
   }
